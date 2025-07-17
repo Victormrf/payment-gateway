681bb90b81383ad900180a7fd169de9a06838187
Victormrf <vmrf2000@hotmail.com>
2025-07-17 03:46:53 -0300

    Added invoice domain, repository, dto and service

--- go-gateway-api/internal/domain/errors.go
+++ go-gateway-api/internal/domain/errors.go
@@ -7,4 +7,6 @@ var (
 	ErrDuplicatedAPIKey = errors.New("api key already exists") // retornado em tentativa de criar quando uma chave de API já existe
 	ErrInvoiceNotFound = errors.New("invoice not found") // retornado quando uma fatura não é encontrada
 	ErrUnauthorizedAccess = errors.New("unauthorized access") // retornado quando o acesso não é autorizado
+	ErrInvalidAmount = errors.New("amount must be greater than 0") // retornado quando o valor da fatura é inválido
+	ErrInvalidStatus = errors.New("invalid status") // retornado quando o status da fatura é inválido
 )
